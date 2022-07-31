fff0e30125a2d54827fb45acd1591c3a8083ff80
tom <tomnygren@guzz.com>
2022-07-31 13:16:51 +0100

    Add update marker functionality

--- programs/customshop/src/lib.rs
+++ programs/customshop/src/lib.rs
@@ -8,33 +8,29 @@ declare_id!("7GAzi1mmd9CT3kgV8vL1RbvQJyTNYRjYfuJ7rV42vVoi");
 pub mod customshop {
     use super::*;
 
-    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-    //     Ok(())
-    // }
-
     pub fn create_marker(ctx: Context<CreateMarker>, domain: String) -> Result<()> {
-     
         let marker = &mut ctx.accounts.marker;
         marker.authority = *ctx.accounts.authority.key;
+        marker.linkedWallet = *ctx.accounts.user.key;
+        marker.domain = domain;
         marker.created = true;
+        Ok(())
+    }
+
+    pub fn update_linked_wallet(ctx: Context<UpdateMarker>, domain: String) -> Result<()> {
+        let marker = &mut ctx.accounts.marker;
+        msg!("{}", *ctx.accounts.user.key);
+        marker.linkedWallet = *ctx.accounts.user.key;
         Ok(())
     }
 }
 
-// #[derive(Accounts)]
-// pub struct Initialize {}
-
-
-// #[account]
-// pub struct MarkerSeeds {
-//     domain: String,
-//     bump: u8,
-// }
-
 #[account]
 pub struct Marker {
-    authority: Pubkey,
-    created: bool
+    authority: Pubkey, /// 32 
+    linkedWallet: Pubkey, /// 32
+    domain: String, /// 36 = 32 + 4  -> (32 IS LIMITED BY SEED LENGTH, 4 GIVES THE LENGTH OF THE STRING)
+    created: bool 
 }
 
 // validation struct for create marker
@@ -46,13 +42,33 @@ pub struct CreateMarker<'info> {
 
     authority: Signer<'info>,
 
-    #[account(init, payer = user, space = 8+32+1, seeds = [b"marker", 
-    domain.as_bytes()
-    
-    ], bump)]
+    #[account(init, payer = user, 
+        space = 8+32+32+36+1, seeds = [
+        b"marker", 
+        domain.as_bytes()
+        ], bump)]
     marker: Account<'info, Marker>,
-
     system_program: Program<'info, System>,
-
 }
+
+// validation struct for create marker
+#[derive(Accounts)]
+#[instruction(domain: String)]
+pub struct UpdateMarker<'info> {
+    #[account(mut)]
+    user: Signer<'info>,
+
+    authority: Signer<'info>,
+
+    #[account(
+        mut, 
+        has_one = authority,
+        seeds = [
+        b"marker", 
+        domain.as_bytes()
+        ], bump)]
+    marker: Account<'info, Marker>,
+    system_program: Program<'info, System>,
+}
+
 
--- tests/customshop.ts
+++ tests/customshop.ts
@@ -1,10 +1,16 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { expect } from "chai";
 import { Customshop } from "../target/types/customshop";
 
-const TEST_DOMAIN = "nft.hedgehog.org";
+const TEST_DOMAIN_SHORT = "nft.hedgehog.org";
+
+const TEST_DOMAIN_LONG = "nft.hedgehogasdljfalsdkjfhal.org";
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 describe("customshop", () => {
   // Configure the client to use the local cluster.
@@ -14,27 +20,33 @@ describe("customshop", () => {
 
   const program = anchor.workspace.Customshop as Program<Customshop>;
 
-  // it("Is initialized", async () => {
-  //   // Add your test here.
-  //   const tx = await program.methods.initialize().rpc();
-  //   console.log("Your transaction signature", tx);
-  // });
-
+  
   it("Creates marker", async () => {
+
+    const keyPair = Keypair.generate()
+
+    const result = await provider.connection.requestAirdrop(keyPair.publicKey, 2 * LAMPORTS_PER_SOL)
+
+    await sleep(500)
+    
+    console.log({result})
+
     const [markerPDA, _] = await PublicKey.findProgramAddress(
       [
         anchor.utils.bytes.utf8.encode("marker"),
-        anchor.utils.bytes.utf8.encode(TEST_DOMAIN),
+        anchor.utils.bytes.utf8.encode(TEST_DOMAIN_LONG),
       ],
       program.programId
     );
 
     const tx = await program.methods
-      .createMarker(TEST_DOMAIN)
+      .createMarker(TEST_DOMAIN_LONG)
       .accounts({
-        user: provider.wallet.publicKey,
+        authority: provider.wallet.publicKey,
+        user: keyPair.publicKey,
         marker: markerPDA,
       })
+      .signers([keyPair])
       .rpc();
     console.log({ tx });
 
@@ -45,5 +57,81 @@ describe("customshop", () => {
     expect(
       (await program.account.marker.fetch(markerPDA)).authority.toBase58()
     ).to.equal(provider.wallet.publicKey.toBase58());
+
+    expect(
+      (await program.account.marker.fetch(markerPDA)).linkedWallet.toBase58()
+    ).to.equal(keyPair.publicKey.toBase58());
+
+
+    expect(
+      (await program.account.marker.fetch(markerPDA)).domain
+    ).to.equal(TEST_DOMAIN_LONG);
+
+
+  });
+
+  it("Updates marker owned", async () => {
+
+    const keyPair = Keypair.generate()
+
+    const result = await provider.connection.requestAirdrop(keyPair.publicKey, 2 * LAMPORTS_PER_SOL)
+
+    await sleep(500)
+    
+    console.log({result})
+
+    const [markerPDA, _] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("marker"),
+        anchor.utils.bytes.utf8.encode(TEST_DOMAIN_LONG),
+      ],
+      program.programId
+    );
+
+
+      /// ensure that the linked wallet starts off as NOT the  new user
+      expect(
+        (await program.account.marker.fetch(markerPDA)).linkedWallet.toBase58()
+      ).to.not.equal(keyPair.publicKey.toBase58());
+  
+
+
+    const tx = await program.methods
+      .updateLinkedWallet(TEST_DOMAIN_LONG)
+      .accounts({
+        authority: provider.wallet.publicKey,
+        user: keyPair.publicKey,
+        marker: markerPDA,
+      })
+      .signers([keyPair])
+      .rpc();
+    console.log({ tx });
+
+    await sleep(500)
+
+    /// ensure that authority still matches
+    expect(
+      (await program.account.marker.fetch(markerPDA)).authority.toBase58()
+    ).to.equal(provider.wallet.publicKey.toBase58());
+
+    /// ensure created is still true
+    expect((await program.account.marker.fetch(markerPDA)).created).to.equal(
+      true
+    );
+
+
+    /// ensure that the linked wallet has switched over to the new user
+    expect(
+      (await program.account.marker.fetch(markerPDA)).linkedWallet.toBase58()
+    ).to.equal(keyPair.publicKey.toBase58());
+
+
+    // /// ensure domain is unchanged
+    expect(
+      (await program.account.marker.fetch(markerPDA)).domain
+    ).to.equal(TEST_DOMAIN_LONG);
+
+
+
   });
 });
