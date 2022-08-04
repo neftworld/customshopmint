1c174112421bf3f2edf9f5a930d375cbd9198ed9
tom <tomnygren@guzz.com>
2022-08-04 12:19:47 +0100

    Improve test structure

--- tests/customshop.ts
+++ tests/customshop.ts
@@ -10,9 +10,7 @@ import {
   createAssociatedTokenAccount,
   transferChecked,
 } from "@solana/spl-token";
-
-const TEST_DOMAIN_1 = "nft.hedgehogasdljfalsdkjfhal.org";
-
+import { before } from "mocha";
 const TEST_DOMAIN_2 = "nft.hedgehog.org";
 
 function sleep(ms) {
@@ -29,6 +27,71 @@ function makeid(length) {
   return result;
 }
 
+async function createNewMint(
+  provider: anchor.Provider,
+  originalMinter: Keypair
+) {
+  const mint = await createMint(
+    provider.connection,
+    originalMinter,
+    originalMinter.publicKey,
+    originalMinter.publicKey,
+    0
+  );
+  await sleep(500);
+
+  const ata = await createAssociatedTokenAccount(
+    provider.connection, // connection
+    originalMinter, // fee payer
+    mint, // mint
+    originalMinter.publicKey // owner,
+  );
+
+  await sleep(500);
+
+  await mintToChecked(
+    provider.connection,
+    originalMinter,
+    mint,
+    ata,
+    originalMinter,
+    1,
+    0
+  );
+  await sleep(500);
+
+  return { mint, ata };
+}
+
+const createDomainAndPDA = async (
+  provider: anchor.Provider,
+  program: anchor.Program<Customshop>
+) => {
+  const testDomain = `${makeid(10)}.hedgehog.com`;
+  const markerPDA = (
+    await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("marker"),
+        anchor.utils.bytes.utf8.encode(testDomain),
+      ],
+      program.programId
+    )
+  )[0];
+  return { testDomain, markerPDA };
+};
+
+const createMinter = async (provider: anchor.Provider) => {
+  const minter = Keypair.generate();
+  await provider.connection.requestAirdrop(
+    minter.publicKey,
+    5 * LAMPORTS_PER_SOL
+  );
+
+  await sleep(500);
+
+  return minter;
+};
+
 describe("Customshop", async () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
@@ -37,99 +100,141 @@ describe("Customshop", async () => {
 
   const program = anchor.workspace.Customshop as Program<Customshop>;
 
-  let originalMinter: Keypair = undefined!;
-
   describe("given original owner Keypair is funded", async () => {
-    beforeEach(async () => {
-      originalMinter = Keypair.generate();
-      await provider.connection.requestAirdrop(
-        originalMinter.publicKey,
-        5 * LAMPORTS_PER_SOL
-      );
-
-      await sleep(500);
-    });
-
     describe(`given that mint has been created and is owned by the claimant`, async () => {
-      let mint: PublicKey | undefined = undefined!;
-      let ata: PublicKey = undefined!;
-      beforeEach(async () => {
-        mint = await createMint(
-          provider.connection,
-          originalMinter,
-          originalMinter.publicKey,
-          originalMinter.publicKey,
-          0
-        );
-        await sleep(500);
+      describe("given that the marker has been created", async () => {
+        let originalMinter: Keypair = undefined!;
+        let mintAndAta: {
+          mint: PublicKey;
+          ata: PublicKey;
+        } = undefined!;
 
-        ata = await createAssociatedTokenAccount(
-          provider.connection, // connection
-          originalMinter, // fee payer
-          mint, // mint
-          originalMinter.publicKey // owner,
-        );
+        let domainAndPDA: {
+          testDomain: string;
+          markerPDA: PublicKey;
+        } = undefined!;
+        before(() => {
+          return new Promise((resolve, reject) => {
+            createMinter(provider).then(async (result) => {
+              originalMinter = result;
 
-        await sleep(500);
+              mintAndAta = await createNewMint(provider, originalMinter);
 
-        await mintToChecked(
-          provider.connection,
-          originalMinter,
-          mint,
-          ata,
-          originalMinter,
-          1,
-          0
-        );
-        await sleep(500);
+              domainAndPDA = await createDomainAndPDA(provider, program);
+
+              await program.methods
+                .createMarker(domainAndPDA.testDomain)
+                .accounts({
+                  authority: provider.wallet.publicKey,
+                  owner: originalMinter.publicKey,
+                  marker: domainAndPDA.markerPDA,
+                  mint: mintAndAta.mint.toBase58(),
+                  tokenAccount: mintAndAta.ata,
+                })
+                .signers([originalMinter])
+                .rpc();
+
+              await sleep(500);
+              resolve(0);
+            });
+            // console.log({provider, originalMinter})
+          });
+        });
+
+        it("marker should have the correct authority", async () => {
+          expect(
+            (
+              await program.account.marker.fetch(domainAndPDA.markerPDA)
+            ).authority.toBase58()
+          ).to.equal(provider.wallet.publicKey.toBase58());
+        });
+        it("marker should have the correct minter", async () => {
+          expect(
+            (
+              await program.account.marker.fetch(domainAndPDA.markerPDA)
+            ).owner.toBase58()
+          ).to.equal(originalMinter.publicKey.toBase58());
+        });
+        it("marker should have the correct domain", async () => {
+          expect(
+            (await program.account.marker.fetch(domainAndPDA.markerPDA)).domain
+          ).to.equal(domainAndPDA.testDomain);
+        });
+        it("marker should have the correct mint", async () => {
+          expect(
+            (
+              await program.account.marker.fetch(domainAndPDA.markerPDA)
+            ).mint.toBase58()
+          ).to.equal(mintAndAta.mint.toBase58());
+        });
+        describe("Given mint has been transferred to a new owner", () => {
+          const newOwner = Keypair.generate();
+          let newAta: PublicKey = undefined;
+
+          beforeEach(async () => {
+            await provider.connection.requestAirdrop(
+              newOwner.publicKey,
+              5 * LAMPORTS_PER_SOL
+            );
+            await sleep(500);
+
+            newAta = await createAssociatedTokenAccount(
+              provider.connection, // connection
+              newOwner, // fee payer
+              mintAndAta.mint, // mint
+              newOwner.publicKey // owner,
+            );
+
+            await transferChecked(
+              provider.connection,
+              newOwner, //payer
+              mintAndAta.ata, // source
+              mintAndAta.mint, // mint
+              newAta, // destination
+              originalMinter, // owner
+              1, //amount
+              0 // decimals
+            );
+
+            await sleep(500);
+          });
+          // it("Claiming the marker by new owner should succeed", async () => {
+          //   const updateLinkedWalletTx = await program.methods
+          //     .updateOwner(domainAndPDA.testDomain)
+          //     .accounts({
+          //       owner: newOwner.publicKey,
+          //       authority: provider.wallet.publicKey,
+          //       marker: domainAndPDA.markerPDA,
+          //       tokenAccount: newAta,
+          //     })
+          //     .signers([newOwner])
+          //     .rpc();
+          //   console.log({ updateLinkedWalletTx });
+
+          //   // await sleep(500);
+
+          //   expect(
+          //     (
+          //       await program.account.marker.fetch(domainAndPDA.markerPDA)
+          //     ).authority.toBase58()
+          //   ).to.equal(provider.wallet.publicKey.toBase58());
+          //   expect(
+          //     (
+          //       await program.account.marker.fetch(domainAndPDA.markerPDA)
+          //     ).owner.toBase58()
+          //   ).to.equal(originalMinter.publicKey.toBase58());
+          //   expect(
+          //     (await program.account.marker.fetch(domainAndPDA.markerPDA))
+          //       .domain
+          //   ).to.equal(domainAndPDA.testDomain);
+          //   expect(
+          //     (
+          //       await program.account.marker.fetch(domainAndPDA.markerPDA)
+          //     ).mint.toBase58()
+          //   ).to.equal(mintAndAta.mint.toBase58());
+          // });
+        });
       });
-
-      describe("given that we have a testdomain and markerPDA", async () => {
-        let testdomain: string = `${makeid(10)}.hedgehog.com`;
-        let markerPDA: PublicKey = undefined;
-        before(async () => {
-          markerPDA = (
-            await PublicKey.findProgramAddress(
-              [
-                anchor.utils.bytes.utf8.encode("marker"),
-                anchor.utils.bytes.utf8.encode(testdomain),
-              ],
-              program.programId
-            )
-          )[0];
-        });
-
-        it("claiming test domain with the mint should succeed", async () => {
-          const createMarkerTx = await program.methods
-            .createMarker(testdomain)
-            .accounts({
-              authority: provider.wallet.publicKey,
-              owner: originalMinter.publicKey,
-              marker: markerPDA,
-              mint: mint.toBase58(),
-              tokenAccount: ata,
-            })
-            .signers([originalMinter])
-            .rpc();
-        });
-
-        it("claiming the same domain for second time should not succeed", async () => {
-          await expect(program.methods
-              .createMarker(testdomain)
-              .accounts({
-                authority: provider.wallet.publicKey,
-                owner: originalMinter.publicKey,
-                marker: markerPDA,
-                mint: mint.toBase58(),
-                tokenAccount: ata,
-              })
-              .signers([originalMinter])
-              .rpc()).to.throw;
-          // }).to.throw(Error);
-        });
-      });
-
-      // xit("create marker as the claimant should succeed", async () => {});
     });
 
     // describe("given that the claimant does not hold the mint", async () => {
