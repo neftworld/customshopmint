c73eddc7b23605db0631d1b4800f66948737c479
tom <tomnygren@guzz.com>
2022-08-08 19:53:14 +0100

    Recover from bad commit

--- programs/customshop/src/lib.rs
+++ programs/customshop/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{TokenAccount, Token, Mint, Burn, CloseAccount};
 //declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-declare_id!("7GAzi1mmd9CT3kgV8vL1RbvQJyTNYRjYfuJ7rV42vVoi");
+declare_id!("5hUjjLKqi5jEMFVgUkz21RwGd6YfQD7uNDHX3GQKcEo6");
 
 #[program]
 pub mod customshop {
@@ -41,51 +41,10 @@ pub mod customshop {
 
         anchor_spl::token::burn(cpi_ctx_burn_mint, 1);
 
-
-
-        // let cpi_accounts_closeaccount = CloseAccount {
-        //     account: marker.to_account_info(),
-        //     destination: ctx.accounts.authority.to_account_info(), /// if called by owner, owner gets the proceeds
-        //     authority: ctx.accounts.authority.to_account_info(),
-        // };
-
-
-        // let cpi_program2 = ctx.accounts.token_program.to_account_info();
-        
-        // let cpi_ctx_close_account = CpiContext::new(cpi_program2, cpi_accounts_closeaccount);
-
-        // anchor_spl::token::close_account(cpi_ctx_close_account);
-
         Ok(())
     }
 
-      /// when mints are replaced (when old has been burned), this can
-      /// be called by anybody
-      pub fn burn_marker_only(ctx: Context<BurnMarker>) -> Result<()> {
-        // let marker = &mut ctx.accounts.marker;
-        msg!("burning token");
-
-        //  if( ctx.accounts.marker.mint.to_account_info() != null) {
-        
-        //  }
-
-       
-        // let cpi_accounts_closeaccount = CloseAccount {
-        //     account: ctx.accounts.marker.to_account_info(),
-        //     destination: ctx.accounts.authority.to_account_info(), /// if called by owner, owner gets the proceeds
-        //     authority: ctx.accounts.authority.to_account_info(),
-        // };
-
-        // let cpi_program = ctx.accounts.token_program.to_account_info();
-
-        
-        // let cpi_ctx_close_account = CpiContext::new(cpi_program, cpi_accounts_closeaccount);
-
-        // anchor_spl::token::close_account(cpi_ctx_close_account);
-        
-
-        Ok(())
-    }
+ 
 }
 
 #[account]
@@ -129,8 +88,6 @@ pub struct CreateMarker<'info> {
     system_program: Program<'info, System>,
 }
 
-
-
 // validation struct for create marker
 #[derive(Accounts)]
 #[instruction(domain: String)]
@@ -158,7 +115,7 @@ pub struct UpdateOwner<'info> {
     token_account: Account<'info, TokenAccount>,
 }
 
-// validation struct for burning when mint exists - can be called by owner only
+// validation struct for burning when mint exists - must be called by owner + authority
 #[derive(Accounts)]
 #[instruction(domain: String)]
 pub struct BurnMarkerAndMint<'info> {
@@ -185,7 +142,6 @@ pub struct BurnMarkerAndMint<'info> {
     #[account(
         mut, 
         has_one = owner,
-        // authority = marker.authority,
         constraint = token_account.owner == owner.key(),
         constraint = marker.mint == token_account.mint,
         constraint = token_account.amount > 0
@@ -193,31 +149,7 @@ pub struct BurnMarkerAndMint<'info> {
     token_account: Account<'info, TokenAccount>,
 
     token_program: Program<'info, Token>,
-
-    // system_program: Program<'info, System>,
 }
 
-
-// validation struct for burning - can only be called when the mint doesn't exist. signed by authority as a cleanup
-// to free up a domain when the associated mint is burned
-#[derive(Accounts)]
-#[instruction(domain: String)]
-pub struct BurnMarker<'info> {
-    #[account(mut)]
-    authority: Signer<'info>,
-
-    #[account(
-        mut, 
-        has_one = authority,
-        seeds = [
-        b"marker", 
-        domain.as_bytes()
-        ], bump)]
-    marker: Account<'info, Marker>,
-
-    token_program: Program<'info, System>,
-
-    system_program: Program<'info, System>,
-}
 
 
